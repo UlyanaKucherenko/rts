5be0c0a65ad9bf649e5a36579234537aa9d6f5fb
UlyanaKucherenko <ulyanakucherenko28@gmail.com>
2022-11-25 16:31:19 +0200

    added eslint && prettier

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,21 +1,20 @@
-import {ITodo} from "../types/data";
+import { ITodo } from 'types/data';
 
 interface ITodoItemProps extends ITodo {
-    toggleTodo: (id: number) => void;
-    removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
 }
 
 const TodoItem: React.FC<ITodoItemProps> = (props) => {
-    const {id, title, complete, toggleTodo, removeTodo} = props;
+  const { id, title, complete, toggleTodo, removeTodo } = props;
 
-    return <div>
-        <input type="checkbox" checked={complete} onChange={() => toggleTodo(id)}/>
-        {title}
-        <button onClick={() => removeTodo(id)}>X</button>
+  return (
+    <div>
+      <input type="checkbox" checked={complete} onChange={() => toggleTodo(id)} />
+      {title}
+      <button onClick={() => removeTodo(id)}>X</button>
     </div>
-}
+  );
+};
 
-export
-{
-    TodoItem
-}
+export { TodoItem };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,21 +1,22 @@
-import {ITodo} from "../types/data";
-import {TodoItem} from "./TodoItem";
+import { ITodo } from 'types/data';
+
+import { TodoItem } from 'components/TodoItem';
 
 interface ITodoListProps {
-    items: ITodo[];
-    toggleTodo: (id: number) => void;
-    removeTodo: (id: number) => void;
+  items: ITodo[];
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
 }
 
 const TodoList: React.FC<ITodoListProps> = (props) => {
-    const {items, toggleTodo, removeTodo} = props;
-    return <div>
-        {
-            items.map((todo) => (
-                <TodoItem key={todo.id} toggleTodo={toggleTodo} removeTodo={removeTodo} {...todo}/>)
-            )
-        }
+  const { items, toggleTodo, removeTodo } = props;
+  return (
+    <div>
+      {items.map((todo) => (
+        <TodoItem key={todo.id} toggleTodo={toggleTodo} removeTodo={removeTodo} {...todo} />
+      ))}
     </div>
-}
+  );
+};
 
-export {TodoList}
+export { TodoList };
